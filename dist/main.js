61ec5435160d7c23763aa7c205c4b785694cde53
rasmus@rhnorskov.com <rasmus@rhnorskov.com>
2017-03-10 15:36:04 +0100

    made into package

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,15 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _qs = require('qs');
+
+var _qs2 = _interopRequireDefault(_qs);
+
 var _url = require('url');
 
 var _url2 = _interopRequireDefault(_url);
@@ -8,23 +18,15 @@ var _cheerio = require('cheerio');
 
 var _cheerio2 = _interopRequireDefault(_cheerio);
 
-var _express = require('express');
-
-var _express2 = _interopRequireDefault(_express);
-
-var _qs = require('qs');
-
-var _qs2 = _interopRequireDefault(_qs);
-
 var _requestPromise = require('request-promise');
 
 var _requestPromise2 = _interopRequireDefault(_requestPromise);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var app = (0, _express2.default)();
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var baseURL = 'http://www.lonelyplanet.com';
+var baseURL = 'https://www.lonelyplanet.com/';
 
 function getEndpoint(id) {
     var type = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 'sights';
@@ -41,47 +43,73 @@ function getEndpoint(id) {
     return _url2.default.resolve(baseURL, 'a/poi-sig/' + id + '?resource=' + encodeURIComponent('/pois?' + query));
 }
 
-function getSights(id) {
-    return (0, _requestPromise2.default)({
-        url: getEndpoint(id),
-        json: true,
-        headers: {
-            'Content-Type': 'application/json'
+var City = function () {
+    function City(id, query) {
+        _classCallCheck(this, City);
+
+        this.id = id;
+        this.query = query;
+    }
+
+    _createClass(City, [{
+        key: 'sights',
+        value: function sights() {
+            var _this = this;
+
+            return new Promise(function (resolve, reject) {
+                var options = {
+                    url: getEndpoint(_this.id),
+                    json: true,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                };
+
+                (0, _requestPromise2.default)(options).then(function (result) {
+                    resolve(result.data.map(function (sight) {
+                        return new Sight({
+                            name: sight.attributes.name,
+                            address: sight.attributes.address.street,
+                            hours_string: sight.attributes.hours_string,
+                            price_string: sight.attributes.price_string,
+                            review: sight.attributes.review
+                        });
+                    }));
+                }).catch(reject);
+            });
         }
-    });
-}
+    }]);
 
-function getCityID(query) {
-    var options = {
-        url: _url2.default.resolve(baseURL, query),
-        transform: function transform(body) {
-            return _cheerio2.default.load(body);
+    return City;
+}();
+
+var Sight = function Sight(parameters) {
+    _classCallCheck(this, Sight);
+
+    Object.assign(this, parameters);
+};
+
+var LonelyPlanet = function () {
+    function LonelyPlanet() {
+        _classCallCheck(this, LonelyPlanet);
+    }
+
+    _createClass(LonelyPlanet, [{
+        key: 'city',
+        value: function city(query) {
+            return new Promise(function (resolve, reject) {
+                (0, _requestPromise2.default)(baseURL + query).then(function (body) {
+                    var $ = _cheerio2.default.load(body);
+                    var href = $('.sights__more').attr('href');
+
+                    if (href) resolve(new City(href.split('/').pop(), query));
+                }).catch(reject);
+            });
         }
-    };
+    }]);
 
-    return new Promise(function (resolve, reject) {
-        (0, _requestPromise2.default)(options).then(function ($) {
-            resolve($('.sights__more').attr('href').split('/').pop());
-        }).catch(reject);
-    });
-}
+    return LonelyPlanet;
+}();
 
-app.get('/', function (req, res) {
-    res.json({
-        name: 'lonely planet api',
-        version: '0.0.1',
-        authors: ['Rasmus Nørskov (rhnorskov)', 'Mathias Wieland (cuntbag)', 'Andreas Sommerset (flapjack)']
-    });
-});
-
-app.get('/:query*', function (req, res) {
-    getCityID(req.params.query + req.params[0]).then(function (id) {
-        return getSights(id);
-    }).then(function (data) {
-        console.log(data.data.length);
-        res.json(data);
-    }).catch(console.error);
-});
-
-app.listen(process.env.port || 3000);
+exports.default = LonelyPlanet;
 //# sourceMappingURL=main.js.map
--- main.js
+++ main.js
@@ -1,12 +1,9 @@
+import qs from 'qs';
 import url from 'url';
 import cheerio from 'cheerio';
-import express from 'express';
-import qs from 'qs';
 import rp from 'request-promise';
 
-const app = express();
-
-const baseURL = 'http://www.lonelyplanet.com';
+const baseURL = 'https://www.lonelyplanet.com/';
 
 function getEndpoint(id, type = 'sights', limit = 1000, offset = 0) {
     const query = qs.stringify({
@@ -19,57 +16,57 @@ function getEndpoint(id, type = 'sights', limit = 1000, offset = 0) {
     return url.resolve(baseURL, `a/poi-sig/${id}?resource=` + encodeURIComponent('/pois?' + query))
 }
 
-function getSights(id) {
-    return rp({
-        url: getEndpoint(id),
-        json: true,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
+class City {
+    constructor(id, query) {
+        this.id = id;
+        this.query = query;
+    }
+
+    sights() {
+        return new Promise((resolve, reject) => {
+            const options = {
+                url: getEndpoint(this.id),
+                json: true,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+
+            rp(options)
+                .then(result => {
+                    resolve(result.data.map(sight => {
+                        return new Sight({
+                            name: sight.attributes.name,
+                            address: sight.attributes.address.street,
+                            hours_string: sight.attributes.hours_string,
+                            price_string: sight.attributes.price_string,
+                            review: sight.attributes.review,
+                        })
+                    }));
+                })
+                .catch(reject)
+        })
+    }
 }
 
-function getCityID(query) {
-    const options = {
-        url: url.resolve(baseURL, query),
-        transform: (body) => {
-            return cheerio.load(body)
-        }
-    };
-
-    return new Promise(function (resolve, reject) {
-        rp(options)
-            .then($ => {
-                resolve($('.sights__more').attr('href').split('/').pop());
-            })
-            .catch(reject)
-    })
+class Sight {
+    constructor(parameters) {
+        Object.assign(this, parameters);
+    }
 }
 
-app.get('/', (req, res) => {
-    res.json({
-        name: 'lonely planet api',
-        version: '0.0.1',
-        authors: [
-            'Rasmus Nørskov (rhnorskov)',
-            'Mathias Wieland (cuntbag)',
-            'Andreas Sommerset (flapjack)'
-        ]
-    })
-});
+export default class LonelyPlanet {
+    city(query) {
+        return new Promise((resolve, reject) => {
+            rp(baseURL + query)
+                .then(body => {
+                    const $ = cheerio.load(body);
+                    const href = $('.sights__more').attr('href');
 
-app.get('/:query*', (req, res) => {
-    getCityID(req.params.query + req.params[0])
-        .then(id => {
-            return getSights(id)
+                    if (href)
+                        resolve(new City(href.split('/').pop(), query));
+                })
+                .catch(reject)
         })
-        .then(data => {
-            console.log(data.data.length);
-            res.json(data)
-        })
-        .catch(console.error);
-});
-
-app.listen(process.env.port || 3000);
-
-
+    }
+}
